8f61581d1d5b9b8790d09a3b722bed0b0bc89344
Basu2006 <basavarajholal26@gmail.com>
2026-01-08 16:27:14 +0530

    Update test_library.py

--- test_library.py
+++ test_library.py
@@ -1,11 +1,11 @@
-from library import library_book_details
-
-def test_library_book_details():
-    expected_output = (
-        "Book Id : 101\n"
-        "Title : Python Programming\n"
-        "Author : Jhon\n"
-        "Year : 2020\n"
+def library_book_details(book_id, title, author, year):
+    return (
+        f"Book Id : {book_id}\n"
+        f"Title : {title}\n"
+        f"Author : {author}\n"
+        f"Year : {year}\n"
     )
 
+
     assert library_book_details(101, "Python Programming", "Jhon", 2020) == expected_output
+    
