c867ba2b13bca45b7ae608f4c3ec15a4159f2a06
Basu2006 <basavarajholal26@gmail.com>
2026-01-08 16:19:34 +0530

    Update library.py

--- library.py
+++ library.py
@@ -7,9 +7,5 @@ def library_book_details(book_id, title, author, year):
     )
 
 if __name__ == "__main__":
-    book_id = ("101")
-    title = ("python Programming")
-    author = ("Jhon")
-    year =  (2020)
 
     print(library_book_details(101,"Python Programming","Jhon", 2020))
